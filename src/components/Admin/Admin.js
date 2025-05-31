65b15b08ed71e19d8f84911587150db24cb7960d
Daxjfol <vietha14122k1@gmail.com>
2025-05-31 08:37:17 +0700

    desgin dashboard

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -4,6 +4,8 @@ import { FaHeart, FaBars } from 'react-icons/fa';
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import PerfectScrollbar from 'react-perfect-scrollbar'
+import Language from "../Header/Language";
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const Admin = (props) => {
     const [collapsed, setCollapsed] = useState(false)
@@ -14,7 +16,16 @@ const Admin = (props) => {
             </div>
             <div className="admin-content">
                 <div className="admin-header">
-                    <FaBars onClick={() => setCollapsed(!collapsed)} />
+                    <span onClick={() => setCollapsed(!collapsed)}> <FaBars className="leftside" /> </span>
+                    <div className="rightside">
+                        <Language />
+                        <NavDropdown title="Setting" id="basic-nav-dropdown">
+                            <NavDropdown.Item >Profile</NavDropdown.Item>
+                            <NavDropdown.Item >Log Out</NavDropdown.Item>
+                        </NavDropdown>
+
+                    </div>
+
                 </div>
                 <div className="admin-main">
                     <PerfectScrollbar>
